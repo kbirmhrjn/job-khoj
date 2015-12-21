543d82614f68d4129d68db1bf786ee2aca70abb3
unknown <kabeerdarocker@gmail.com>
2015-12-21 09:15:41 +0545

    Model & Model Seeder

---
+++ database/seeds/CategorySeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use GuzzleHttp\Client;
+
+use App\Category;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $client = new Client();
+        // $res = $client->get('https://www.kimonolabs.com/api/2qabyeoe?apikey=cQBrKjnK5fz9c0GBVnVm1Gv94s2vPZ8H');
+        // $categories = json_decode($res->getBody(),true);
+        $categories = Cache::get('client');
+        foreach ($categories['results']['collection1'] as $category) {
+            $title = $category['job_categories']['text'];
+            Category::create(['title' => $title, 'alias' => $title, 'slug' => str_slug($title) ]);
+        }
+    }
+}
