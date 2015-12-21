143951d9f1671fcd01d1703b60cfac7dba3d24db
unknown <kabeerdarocker@gmail.com>
2015-12-21 09:16:22 +0545

    App Service Provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Providers;
-
+use Faker\Factory;
+use Faker\Generator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->singleton(Generator::class, function () {
+                return Factory::create('ne_NP');
+            });
     }
 
     /**
