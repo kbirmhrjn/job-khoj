3488b9a4404e3f272d77828b2c98925ce8014359
unknown <kabeerdarocker@gmail.com>
2015-12-23 09:23:05 +0545

    master layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -18,42 +18,51 @@
 </head>
 <body>
     <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Job Khoj</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="#">About</a>
-                    </li>
-                    <li>
-                        <a href="#">Services</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
-
+    <header>
+      {{-- <nav class="navbar navbar-inverse no_radius" role="navigation"> --}}
+          <div class="container">
+            <h1><a href="/"><img src="/img/logo.png?cache" alt="Logo" id="logo" /></a></h1>
+              <!-- Brand and toggle get grouped for better mobile display -->
+              <div class="navbar-header">
+                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu-navigation">
+                      <span class="sr-only">Toggle navigation</span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                  </button>
+              </div>
+              <!-- Collect the nav links, forms, and other content for toggling -->
+              <nav class="collapse navbar-collapse" id="menu-navigation">
+                  <ul class="nav navbar-nav">
+                      <li>
+                          <a href="#">About</a>
+                      </li>
+                      <li>
+                          <a href="#">Services</a>
+                      </li>
+                      <li>
+                          <a href="#">Contact</a>
+                      </li>
+                  </ul>
+              </nav>
+              <!-- /.navbar-collapse -->
+          </div>
+          <!-- /.container -->
+      {{-- </nav> --}}
+    </header>
     <!-- Page Content -->
     <div class="container">
         <div class="row">
             <!-- Blog Entries Column -->
             <div class="col-md-8">
+                <div class="well">
+                    <form>
+                    <div class="form-group no_margin">
+                        <input type="text" class="form-control" placeholder="Find title, company, job, type anything.">
+                    </div>
+                    </form>
+                    <!-- /.input-group -->
+                </div>
                 <h1 class="page-header">
                     Page Heading
                     <small>Secondary Text</small>
@@ -121,21 +130,6 @@
 
             <!-- Blog Sidebar Widgets Column -->
             <div class="col-md-4">
-
-                <!-- Blog Search Well -->
-                <div class="well">
-                    <h4>Blog Search</h4>
-                    <div class="input-group">
-                        <input type="text" class="form-control">
-                        <span class="input-group-btn">
-                            <button class="btn btn-default" type="button">
-                                <span class="glyphicon glyphicon-search"></span>
-                        </button>
-                        </span>
-                    </div>
-                    <!-- /.input-group -->
-                </div>
-
                 <!-- Blog Categories Well -->
                 <div class="well">
                     <h4>Categories</h4>
@@ -171,29 +165,20 @@
 
         </div>
         <!-- /.row -->
-
         <hr>
-
         <!-- Footer -->
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2014</p>
+                    <p>Copyright &copy; Job Khoj 2015</p>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
             <!-- /.row -->
         </footer>
-
     </div>
     <!-- /.container -->
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
+    <!-- ApplicationJs -->
+    <script src="js/app.js"></script>
 </body>
-
 </html>
