a618b4aacb462ec3311b855c5741bbebbb521f61
unknown <kabeerdarocker@gmail.com>
2015-12-24 21:20:26 +0545

    gulpjs updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,13 @@
 var elixir = require('laravel-elixir');
-
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
-
+var paths = {
+                'jquery' : './node_modules/jquery/',
+                'bootstrap': './node_modules/bootstrap-sass/assets/'
+            };
 elixir(function(mix) {
-    mix.sass('app.scss');
+  mix.sass("app.scss", 'public/css/', {includePaths: [paths.bootstrap + 'stylesheets/']})
+  .browserify('app.js')
+      .copy(paths.bootstrap + 'fonts/bootstrap/**', 'public/fonts/bootstrap')
+      .browserSync({
+        proxy: 'job-khoj.app'
+      });
 });
